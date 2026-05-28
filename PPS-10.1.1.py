7a1ac8d42a0f1e2c428495cdb0d9a498ce877919
soham-samudra <soham.samudra.batch2025@sitnagpur.siu.edu.in>
2026-05-28 19:17:49 +0530

    Update PPS-10.1.1.py

--- PPS-10.1.1.py
+++ PPS-10.1.1.py
@@ -1,1 +1,12 @@
+# Read input
+s = input()
 
+# Keep only letters, digits, and spaces
+result = ""
+
+for ch in s:
+    if ch.isalnum() or ch == " ":
+        result += ch
+
+# Print result
+print(result)
