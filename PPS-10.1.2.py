a1cb42c0823021f86ed6f2180a114a4c1bd9e821
soham-samudra <soham.samudra.batch2025@sitnagpur.siu.edu.in>
2026-05-28 19:18:08 +0530

    Update PPS-10.1.2.py

--- PPS-10.1.2.py
+++ PPS-10.1.2.py
@@ -1,1 +1,8 @@
+def reverse_string(s):
+	result=s[::-1] 
+	return result
 
+user_input = input("Enter a string: ")
+result = reverse_string(user_input)
+print(f"Original String: {user_input}")
+print(f"Reversed String: {result}")
