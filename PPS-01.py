3ea37f7d332c172ca9ed88591db98cc504f67c5c
soham-samudra <soham.samudra.batch2025@sitnagpur.siu.edu.in>
2026-05-28 19:04:06 +0530

    Create PPS-01.py

---
+++ PPS-01.py
@@ -0,0 +1,37 @@
+
+radius = float(input())
+area = 3.14 * (radius ** 2)
+print(f"{area:.4f}")
+
+
+length = float(input())
+width = float(input())
+
+area = length * width
+print(f"{area:.2f}")
+
+
+side_length = int(input())
+
+area = side_length ** 2
+
+print(f"{area}")
+
+
+
+base = float(input())
+height = float(input())
+
+area =  0.5 * base * height
+print(f"{area:.2f}")
+
+
+marks = int(input())
+
+if marks >= 40:
+	print("Pass")
+else:
+	print("Fail")
+
+
+
